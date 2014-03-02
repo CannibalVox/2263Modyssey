48cda8b1d7a795f2b27ee33030c0b5d76825ef66
CannibalVox <sdbaynham@gmail.com>
2014-03-02 00:00:45 -0600

    Initial checkin

---
+++ src/main/java/com/smptextures/modyssey2263/Mod2263.java
@@ -0,0 +1,34 @@
+/*
+        This mod is 2263, a mod to add many high-quality sci fi cosmetic blocks.
+        Copyright (C) 2014, Sean Pagal
+
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package com.smptextures.modyssey2263;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInterModComms;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Mod2263.MODID, version = Mod2263.VERSION, dependencies="required-after:technicblocks")
+public class Mod2263 {
+    public static final String MODID = "2263modyssey";
+    public static final String VERSION = "1.0";
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        FMLInterModComms.sendMessage("technicblocks", "loadBlocks", "/assets/2263modyssey/allBlocks.json");
+    }
+}
