975ac55eae25a9946384c4f8ffb7b700246b32ed
CannibalVox <sdbaynham@gmail.com>
2014-03-02 01:37:39 -0600

    Remove IMC stuff.

--- src/main/java/com/smptextures/modyssey2263/Mod2263.java
+++ src/main/java/com/smptextures/modyssey2263/Mod2263.java
@@ -19,16 +19,9 @@
 package com.smptextures.modyssey2263;
 
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.event.FMLInterModComms;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Mod2263.MODID, version = Mod2263.VERSION, dependencies="required-after:technicblocks")
 public class Mod2263 {
     public static final String MODID = "2263modyssey";
     public static final String VERSION = "1.0";
-
-    @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-        FMLInterModComms.sendMessage("technicblocks", "loadBlocks", "/assets/2263modyssey/allBlocks.json");
-    }
 }
